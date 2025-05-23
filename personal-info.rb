7bcddcb307b25ef6894b9e96043bdc0c8fe92e89
Meghana <meghanamanchal@gmail.com>
2025-05-23 14:12:09 +0530

    created contact list using array iteration

--- personal-info.rb
+++ personal-info.rb
@@ -1,12 +1,9 @@
-
-#input taking for personal_info
 name = "Meghana M"
 age = 20
 city = "Bangalore"
 college = "Apollo University"
 course = "B.Tech - Kalvium"
 
-# Print personal_info
 puts "Student Information:"
 puts "Name: #{name}"
 puts "Age: #{age}"
